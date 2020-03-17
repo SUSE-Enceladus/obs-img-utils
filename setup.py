7c516dc0c3284696ee20a47a482dcd5cd1793479
Sean Marlow <sean.marlow@suse.com>
2020-03-17 13:06:17 -0500

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('requirements-dev.txt') as req_file:
 
 setup(
     name='obs-img-utils',
-    version='0.1.1',
+    version='0.1.2',
     description="Package provides utils for images in Open Build Service.",
     long_description=readme,
     long_description_content_type="text/markdown",
