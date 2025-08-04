cadcc8a656356127de9fc8aae3d95aedee580c56
Sean Marlow <sean.marlow@suse.com>
2025-08-04 11:08:51 -0400

    Bump version: 1.9.0 → 1.10.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE Public Cloud Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.9.0'
+release = '1.10.0'
 
 
 # -- General configuration ---------------------------------------------------
