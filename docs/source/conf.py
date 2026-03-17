bbc3a1604cd1dff1a6f1ff48d06e557c0fa4986d
Andres Pozo <andres.munoz@suse.com>
2026-03-17 12:50:39 +0100

    Bump version: 1.11.0 → 1.12.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'SUSE Public Cloud Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.11.0'
+release = '1.12.0'
 
 
 # -- General configuration ---------------------------------------------------
